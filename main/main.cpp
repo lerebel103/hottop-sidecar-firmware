4231b6dcc2bb3eb26ea87c2599b16bf861bb7dce
Will Castelnau <lerebel103@gmail.com>
2023-04-08 19:17:00 +0800

    Cleanup of max31850 integration.

--- main/main.cpp
+++ main/main.cpp
@@ -7,7 +7,6 @@ extern "C" {
 #include <esp_system.h>
 }
 
-#include <driver/temp_sensor.h>
 #include <driver/gpio.h>
 #include "level_shifter.h"
 #include "panel_inputs.h"
@@ -21,29 +20,14 @@ extern "C" {
 #define TAG  "main"
 
 #define MAX_SECONDARY_HEAT_RATIO 0.5f;
+#define ONEWIRE_PIN GPIO_NUM_2
+
 
 static ssr_ctrl_t s_ssr1;
 static ssr_ctrl_t s_ssr2;
 static bool _go = false;
+static uint64_t s_max31850_addr = 0;
 
-static void _read_onboard_temp() {
-    temp_sensor_config_t temp_sensor = {
-            .dac_offset = TSENS_DAC_L2,
-            .clk_div = 6,
-    };
-    temp_sensor_set_config(temp_sensor);
-    temp_sensor_start();
-    static int num_readings = 50;
-    double temp = 0;
-    for (int i = 0; i < num_readings; i++) {
-        vTaskDelay(10 / portTICK_RATE_MS);
-        float tsens_out;
-        temp_sensor_read_celsius(&tsens_out);
-        temp += tsens_out;
-    }
-    ESP_LOGI(TAG, "Internal temp: %fC", temp / num_readings);
-    temp_sensor_stop();
-}
 
 static void _start_control_loop() {
     // Turn off heat
@@ -52,12 +36,37 @@ static void _start_control_loop() {
 
     while (_go) {
         double balance = balance_read_percent();
-        double input_duty = power_duty_get();
-        double output_duty = input_duty * balance / 100 * MAX_SECONDARY_HEAT_RATIO;
-        ESP_LOGI(TAG, "Input=%f, Balance: %f, Output=%f", input_duty, balance, output_duty);
+        double input_duty = 0;
+        double output_duty = 0;
 
+        // Get TC value
+        max31850_data_t elm_temp = max31850_read(ONEWIRE_PIN, s_max31850_addr);
+        if (elm_temp.is_valid) {
+            if (elm_temp.thermocouple_status == MAX31850_TC_STATUS_OK) {
+                ESP_LOGI(TAG, "Thermocouple=%fC, Board=%fC", elm_temp.thermocouple_temp, elm_temp.junction_temp);
+
+                input_duty = power_duty_get();
+                output_duty = input_duty * balance / 100 * MAX_SECONDARY_HEAT_RATIO;
+
+            } else {
+                if (elm_temp.thermocouple_status & MAX31850_TC_STATUS_OPEN_CIRCUIT) {
+                    ESP_LOGE(TAG, "Unable to run, thermocouple fault OPEN CIRCUIT");
+                } else if (elm_temp.thermocouple_status & MAX31850_TC_STATUS_SHORT_GND) {
+                    ESP_LOGE(TAG, "Unable to run, thermocouple fault SHORT TO GROUND");
+                } else if (elm_temp.thermocouple_status & MAX31850_TC_STATUS_SHORT_VCC) {
+                    ESP_LOGE(TAG, "Unable to run, thermocouple fault SHORT TO VCC");
+                }
+            }
+        } else {
+            ESP_LOGE(TAG, "Unable to run, error reading thermocouple data.");
+        }
+
+
+        ESP_LOGI(TAG, "Input=%f, Balance: %f, Output=%f", input_duty, balance, output_duty);
         ssr_ctlr_set_duty(s_ssr1, input_duty);
         ssr_ctlr_set_duty(s_ssr2, output_duty);
+
+
         vTaskDelay(pdMS_TO_TICKS(500));
     }
 
@@ -68,17 +77,18 @@ extern "C" void app_main() {
     gpio_install_isr_service(0);
     sync_signal_generator_init(GPIO_NUM_34, RMT_CHANNEL_3);
 
-    _read_onboard_temp();
     level_shifter_init();
     level_shifter_enable(true);
     panel_inputs_init();
-    max31850_init();
     balancer_init();
+    max3185_devices_t found_devices = max31850_list(ONEWIRE_PIN);
+    ESP_ERROR_CHECK(found_devices.devices_address_length == 1 ? ESP_OK : ESP_FAIL);
+    s_max31850_addr = found_devices.devices_address[0];
 
     // Init SSRs
-    s_ssr1 = {.gpio = GPIO_NUM_10, RMT_CHANNEL_0, MAINS_50HZ, 0};
+    s_ssr1 = {.gpio = GPIO_NUM_10, .channel = RMT_CHANNEL_0, .mains_hz= MAINS_50HZ, .duty = 0};
     ssr_ctlr_init(s_ssr1);
-    s_ssr2 = {.gpio = GPIO_NUM_11, RMT_CHANNEL_1, MAINS_50HZ, 0};
+    s_ssr2 = {.gpio = GPIO_NUM_11, .channel = RMT_CHANNEL_1, .mains_hz= MAINS_50HZ, .duty = 0};
     ssr_ctlr_init(s_ssr2);
 
     // Init reading inbound power duty
--- main/max31850.h
+++ main/max31850.h
@@ -1,3 +1,57 @@
 #pragma once
 
-void max31850_init();
+
+#include <cstdint>
+#include <hal/gpio_types.h>
+
+#define MAX31850_TC_STATUS_OK           0x0
+#define MAX31850_TC_STATUS_OPEN_CIRCUIT 0x1
+#define MAX31850_TC_STATUS_SHORT_GND    0x2
+#define MAX31850_TC_STATUS_SHORT_VCC    0x4
+
+#define MAX31850_NUM_DEVICES_MAX 8
+
+struct max3185_devices_t {
+    uint64_t devices_address[MAX31850_NUM_DEVICES_MAX];
+    uint8_t devices_address_length;
+} ;
+
+struct max31850_data_t {
+
+    /**
+     * Tells if a read was successful and no CRC errors were encountered.
+     * The temperature values are undefined when this flag is false.
+     */
+    bool is_valid;
+
+    /**
+     * Bitfield flag representing the status of the thermocouple.
+     * MAX31850_TC_STATUS_OK returned when operating correctly.
+     * See MAX31850_TC_STATUS_*
+     */
+    uint8_t thermocouple_status;
+
+    /**
+     * Temperature read from the connected thermocouple.
+     */
+    float thermocouple_temp;
+
+    /**
+     * Temperature read from the onboard sensor, representing the junction temperature.
+     */
+    float junction_temp;
+};
+
+/**
+ * Reads the current status and temperature data.
+ * @param one_wire_pin Pin on which the 1-wire device is attached.
+ * @param device_address 1-wire address for the desired device
+ * @return
+ */
+max31850_data_t max31850_read(gpio_num_t one_wire_pin, uint64_t device_address);
+
+/**
+ * Lists the number of max31850 devices found.
+ * @param one_wire_pin Pin on which 1-wire devices are attached to.
+ */
+max3185_devices_t max31850_list(gpio_num_t one_wire_pin);
