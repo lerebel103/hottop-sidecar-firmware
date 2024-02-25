cb46d0f73f6ec888db96c22849cdd496de78b68c
Will Castelnau <lerebel103@gmail.com>
2024-02-25 21:19:35 +0800

    Added fw build in docker image

--- ci/lib/BuildFWResourcesStack.ts
+++ ci/lib/BuildFWResourcesStack.ts
@@ -1,22 +1,30 @@
 import * as cdk from "aws-cdk-lib";
-import { Construct } from 'constructs';
-import {Bucket} from "aws-cdk-lib/aws-s3";
+import {Construct} from 'constructs';
 import {CodeBuildStep, CodePipeline} from "aws-cdk-lib/pipelines";
 import {FileSet, IFileSetProducer} from "aws-cdk-lib/pipelines/lib/blueprint/file-set";
-
-const myCachingBucket : Bucket | undefined = undefined;
+import {ComputeType, LinuxBuildImage, LocalCacheMode} from "aws-cdk-lib/aws-codebuild";
+import {Cache, BuildSpec} from "aws-cdk-lib/aws-codebuild";
 
 export class BuildFWResourcesStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: cdk.StackProps, sourceFiles: FileSet | undefined) {
         super(scope, id, props);
 
+
+
         const pipeline = new CodePipeline(this, 'build-fw', {
+            // Turn this on because the pipeline uses Docker image assets
+            dockerEnabledForSelfMutation: true,
             pipelineName: 'build-fw',
             synth: new CodeBuildStep('build', {
+                cache: Cache.local(LocalCacheMode.DOCKER_LAYER, LocalCacheMode.CUSTOM),
                 input: sourceFiles,
                 commands: ['ls -tral'],
+                buildEnvironment: {
+                    buildImage: LinuxBuildImage.fromDockerRegistry('espressif/idf:v5.2'),
+                    computeType: ComputeType.SMALL,
+                    privileged: true
+                }
             })
         });
-
     }
 }
