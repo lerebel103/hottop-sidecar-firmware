d58d4fb437b1ed3df8d71cab039692cede297e14
Will Castelnau <lerebel103@gmail.com>
2024-02-25 21:38:13 +0800

    another try

--- ci/lib/BuildFWResourcesStack.ts
+++ ci/lib/BuildFWResourcesStack.ts
@@ -5,6 +5,8 @@ import {FileSet, IFileSetProducer} from "aws-cdk-lib/pipelines/lib/blueprint/fil
 import {ComputeType, LinuxBuildImage, LocalCacheMode} from "aws-cdk-lib/aws-codebuild";
 import {Cache, BuildSpec} from "aws-cdk-lib/aws-codebuild";
 
+const outDir = 'ci/cdk.out';
+
 export class BuildFWResourcesStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: cdk.StackProps, sourceFiles: FileSet | undefined) {
         super(scope, id, props);
@@ -14,12 +16,12 @@ export class BuildFWResourcesStack extends cdk.Stack {
         const pipeline = new CodePipeline(this, 'build-fw', {
             // Turn this on because the pipeline uses Docker image assets
             dockerEnabledForSelfMutation: true,
-            pipelineName: 'build-fw',
+            pipelineName: 'hottopsidecar-build-fw',
             synth: new CodeBuildStep('build', {
                 cache: Cache.local(LocalCacheMode.DOCKER_LAYER, LocalCacheMode.CUSTOM),
-                input: sourceFiles,
-                commands: ['ls -tral'],
-                primaryOutputDirectory: 'ci/cdk.out',
+                input: sourceFiles?.primaryOutput,
+                commands: ['ls -tral', `mkdir -p ${outDir}`],
+                primaryOutputDirectory: outDir,
                 buildEnvironment: {
                     buildImage: LinuxBuildImage.fromDockerRegistry('espressif/idf:v5.2'),
                     computeType: ComputeType.SMALL,
