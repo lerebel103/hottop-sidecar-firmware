ebdc9206b64e7a534ce7bce6be2f9acb33833dac
Will Castelnau <lerebel103@gmail.com>
2024-03-05 21:56:28 +0800

    Added iam user for github actions

--- ci/lib/FirmwareResourcesStack.ts
+++ ci/lib/FirmwareResourcesStack.ts
@@ -10,13 +10,14 @@ import {Repository} from "aws-cdk-lib/aws-codecommit";
 import {BlockPublicAccess, Bucket, BucketEncryption} from "aws-cdk-lib/aws-s3";
 import {Globals} from "./globals";
 import {ReadWriteType, Trail} from "aws-cdk-lib/aws-cloudtrail";
-import {aws_codepipeline_actions} from "aws-cdk-lib";
+import {aws_codepipeline_actions, aws_iam, CfnOutput} from "aws-cdk-lib";
+import {CfnAccessKey, Effect} from "aws-cdk-lib/aws-iam";
 
 export class FirmwareResourcesStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props: cdk.StackProps, sourceFiles: CodePipelineSource) {
         super(scope, id, props);
 
-        // Define the bucket used to receive
+        // Define the bucket used to receive new firmware builds from GitHub Actions
         const otaBucket = new Bucket(this, `afr-ota-${Globals.THING_MANUFACTURER}-${Globals.THING_TYPE_NAME}-${Globals.STAGE_NAME}`, {
             bucketName: `afr-ota-${Globals.THING_MANUFACTURER}-${Globals.THING_TYPE_NAME}-${Globals.STAGE_NAME}`,
             blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
@@ -25,6 +26,25 @@ export class FirmwareResourcesStack extends cdk.Stack {
             versioned: true,
             removalPolicy: cdk.RemovalPolicy.RETAIN,
         });
+
+        // User that will allow GitHub to push firmware to the ota bucket
+        const otaBucketUser = new aws_iam.User(this, 'hottopsidecar-ota-bucket-user', {
+            userName: 'hottopsidecar-ota-bucket-user',
+        });
+        otaBucketUser.addToPolicy(new aws_iam.PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ['s3:PutObject'],
+                resources: [otaBucket.bucketArn]
+            })
+        );
+
+        // Add secret to the user
+        const accessKey = new CfnAccessKey(this, 'CfnAccessKey', {
+            userName: otaBucketUser.userName,
+        });
+        // Output the auth info as part of cloud formation output
+        new CfnOutput(this, 'accessKeyId', {value: accessKey.ref});
+        new CfnOutput(this, 'secretAccessKey', {value: accessKey.attrSecretAccessKey});
 
         // Now monitor for inbound changes on new firmwares landing in the bucket
         const sourceOutput = new Artifact();
