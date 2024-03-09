2590dcb8a42c9bbb5907d0c0260bf91389f4488e
Will Castelnau <lerebel103@gmail.com>
2024-03-09 21:41:57 +0800

    Fixed incorrect zip file in github action

--- ci/lib/FirmwareDeployStack.ts
+++ ci/lib/FirmwareDeployStack.ts
@@ -2,7 +2,14 @@ import * as cdk from "aws-cdk-lib";
 import {Construct} from 'constructs';
 import {CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 import {FileSet} from "aws-cdk-lib/pipelines/lib/blueprint/file-set";
-import {ComputeType, LinuxBuildImage, LocalCacheMode, Project, Source} from "aws-cdk-lib/aws-codebuild";
+import {
+    ComputeType,
+    LinuxBuildImage,
+    LocalCacheMode,
+    PipelineProject,
+    Project,
+    Source
+} from "aws-cdk-lib/aws-codebuild";
 import {BuildSpec} from "aws-cdk-lib/aws-codebuild";
 import {Artifact, Pipeline, PipelineType} from "aws-cdk-lib/aws-codepipeline";
 import {CodeBuildAction, S3SourceAction, S3Trigger} from "aws-cdk-lib/aws-codepipeline-actions";
@@ -10,6 +17,7 @@ import {BlockPublicAccess, Bucket, BucketEncryption} from "aws-cdk-lib/aws-s3";
 import {Globals} from "./globals";
 import {ReadWriteType, Trail} from "aws-cdk-lib/aws-cloudtrail";
 import {ParameterDataType, ParameterTier, StringParameter} from "aws-cdk-lib/aws-ssm";
+import {S3ImportSource} from "aws-cdk-lib/aws-cloudfront";
 
 const OTA_BUCKET_PARAMETER_URI = `/iot/rebelthings/hottopsidecar/ota-bucket`;
 
@@ -75,67 +83,24 @@ export class FirmwareDeployStack extends cdk.Stack {
             actions: [sourceAction],
         });
 
-        // Sign Firmware
-        const signAction = new CodeBuildAction({
-            actionName: 'SignFirmware',
-            project: new Project(this, 'SignFirmware', {
-                projectName: 'SignFirmware',
-                buildSpec: BuildSpec.fromObject({
-                    version: '0.2',
-                    phases: {
-                        install: {
-                            commands: [
-                                'echo "Signing firmware to device"',
-                            ],
-                        },
-                        build: {
-                            commands: [
-                                'echo "Firmware Signed to device"',
-                            ],
-                        },
-                    },
-                }),
-                environment: {
-                    buildImage: LinuxBuildImage.STANDARD_5_0,
-                    computeType: ComputeType.SMALL,
-                },
-            }),
-            input: sourceOutput,
+        // Create a CodeBuild project
+        const project = new PipelineProject(this, 'hottopsidecar-fw-project', {
+            description: 'Deploy firmware for OTA Jobs',
+            environment: {
+                computeType: ComputeType.SMALL,
+            },
+            buildSpec: BuildSpec.fromSourceFilename('deploy/buildspec.yaml')
         });
 
-
-        // Deploy the firmware to the device
+        // Sign Firmware
         const deployAction = new CodeBuildAction({
             actionName: 'DeployFirmware',
-            project: new Project(this, 'DeployFirmware', {
-                projectName: 'DeployFirmware',
-                buildSpec: BuildSpec.fromObject({
-                    version: '0.2',
-                    phases: {
-                        install: {
-                            commands: [
-                                'echo "Deploying firmware to device"',
-                                'ls -tral'
-                            ],
-                        },
-                        build: {
-                            commands: [
-                                'echo "Firmware deployed to device"',
-                            ],
-                        },
-                    },
-                }),
-                environment: {
-                    buildImage: LinuxBuildImage.STANDARD_5_0,
-                    computeType: ComputeType.SMALL,
-                },
-            }),
+            project: project,
             input: sourceOutput,
         });
         
-        
-        pipeline.addStage({stageName: 'deploy-firmware-OTA', actions: [signAction, deployAction]});
-
+        pipeline.addStage({stageName: 'deploy-firmware-OTA', actions: [deployAction]});
 
     }
+
 }
