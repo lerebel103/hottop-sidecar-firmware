3b534863af0b8062f2281b479c4950a97b6bcd59
Will Castelnau <lerebel103@gmail.com>
2024-03-10 18:18:16 +0800

    Fixed reference from py to sh script to sign firmware

--- ci/bin/sign-firmware.sh
+++ ci/bin/sign-firmware.sh
@@ -16,14 +16,17 @@ destination_folder_key=$3
 code_signing_profile=$(echo "$4" | sed -e "s/.*\///g")
 
 # extract firmware version
-ver=$(cat deploy/firwmare/version.txt)
+firmware_version=$(cat ./deploy/firwmare/firmware_version.txt)
+hardware_revision=$(cat ./deploy/firwmare/hardware_revision.txt)
+source_firmware_key=$destination_folder_key/$firmware_version/$hardware_revision/hottop-sidecar-firmware.bin
+signed_firmware_key=$destination_folder_key/$firmware_version/$hardware_revision/signed/hottop-sidecar-firmware.bin
 
 # First we copy the firmware files to the target folders
-aws s3 cp -r $local_firmware_folder/* s3://$bucket_name/$destination_folder_key/$ver/
+aws s3 cp -r $local_firmware_folder/* s3://$bucket_name/
 
 # Sign the firmware file
 #echo "Signing the firmware file..."
-#aws signer start-signing-job \
-#    --source "{\"s3\":{\"bucketName\":\"$bucket_name\", \"key\":\"$firmware_file\", \"version\":\"latest\"}}" \
-#    --destination "{\"s3\":{\"bucketName\":\"$bucket_name\", \"prefix\":\"$destination_file\"}}" \
-#    --profile-name $code_signing_profile
+aws signer start-signing-job \
+    --source "{\"s3\":{\"bucketName\":\"$bucket_name\", \"key\":\"$source_firmware_key\", \"version\":\"latest\"}}" \
+    --destination "{\"s3\":{\"bucketName\":\"$bucket_name\", \"prefix\":\"$signed_firmware_key\"}}" \
+    --profile-name "$code_signing_profile"
