927021295c55e698d5c04f7cd38ef3f71fd0b20a
Will Castelnau <lerebel103@gmail.com>
2024-03-12 15:06:01 +0800

    Fixed up reference to GH vars

--- ci/lib/FirmwareStage.ts
+++ ci/lib/FirmwareStage.ts
@@ -9,11 +9,15 @@ export class FirmwareStage extends Stage {
 
     const fwResources = new FirmwareDeployStack(this, "build-stack", props);
 
+    const owner = process.env.GH_OWNER || "";
+    const repo = process.env.GH_REPOSITORY || "";
+    if (owner.length == 0 || repo.length == 0) {
+      throw new Error("GH_OWNER and GH_REPOSITORY must be set for desired GitHub connection.");
+    }
+
     // Install GitHub Actions OIDC auth
     const ghProps = {
-      repositoryConfig: [
-        { owner: "lerebel103", repo: "hottop-sidecar-firmware" },
-      ],
+      repositoryConfig: [{ owner, repo }],
       otaBucketArn: fwResources.otaBucketArn,
     };
     new GithubActionsAwsStack(this, "auth-stack", ghProps);
